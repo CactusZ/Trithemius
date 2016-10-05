4a9c2be1d28fbd6d0232daf6637e3a253a755b24
CactusZ <igorkasevich@gmail.com>
2016-10-05 11:11:55 +0000

    closes #3

--- Java/src/steganography_tool/Steganography_Tool.java
+++ Java/src/steganography_tool/Steganography_Tool.java
@@ -30,62 +30,76 @@ public class Steganography_Tool {
     */
     public static void main(String[] args) throws IOException, WavFileException {
         // TODO code application logic here
-        int choice;
-        choice = args[0].equals("Hide") ? 1 : 2;
-        FileInputStream in = null;
-        FileOutputStream out = null;
-        WavFile fileIn = null;
-        WavFile fileOut = null;
-        SteganographyWav stegoWav = new SteganographyWav();
-        try {
-            if (choice == 1) {
-                fileIn = WavFile.openWavFile(new File(args[3]));
-                String fileOutName = new String(args[4]);
-
-                fileOut = WavFile.newWavFile(new File(fileOutName), fileIn.getNumChannels(), fileIn.getNumFrames(), fileIn.getValidBits(), fileIn.getSampleRate());
-                
-                int choice2 = args[1].equals("string") ? 1 : 2;
-
-                if (choice2 == 1) {
-                    String message = new String(args[2]);
-                    stegoWav.encryptMessage(message , fileIn, fileOut);
-                } else if (choice2 == 2) {
-                    in = new FileInputStream(args[2]);
-
-                    if (in.getChannel().size() * 8 > fileIn.getNumFrames()) {
-                        System.out.println("sorry, for this .wav file, maximum size is " + fileIn.getNumFrames() / 8);
-                        return;
-                    }
-                    stegoWav.encryptMessage(in, fileIn, fileOut);
-                }
-            } else if (choice == 2) {
-                fileIn = WavFile.openWavFile(new File(args[2]));
-                int choice2 = args[1].equals("string") ? 1 : 2;
-                
-                if (choice2 == 1) {
-                   System.out.println(stegoWav.decryptMessage(fileIn));
-                } else if (choice2 == 2) {
-                    out = new FileOutputStream(args[3]);
-                    stegoWav.decryptMessage(fileIn, out);
-                }
-            }
-
-        } finally {
-            if (fileIn != null) {
-                fileIn.close();
-            }
-            if (fileOut != null) {
-                fileOut.close();
-            }
-            if (in != null) {
-                in.close();
-            }
-            if (out != null) {
-                out.close();
-            }
-
+        
+       
+        int choice = args[0].equals("Hide") ? 1 : 2;
+        
+        /*detect format - WAV or MP3*/
+        String extension = "";
+        int i = args[4-choice].lastIndexOf('.'); // if choice = 1 (Hide) 4-1=3, else 4-2=2
+        if (i > 0) {
+            extension = args[4-choice].substring(i+1);
         }
         
+        if (extension.equals("wav")) {
+            
+            FileInputStream in = null;
+            FileOutputStream out = null;
+            WavFile fileIn = null;
+            WavFile fileOut = null;
+            SteganographyWav stegoWav = new SteganographyWav();
+            try {
+                if (choice == 1) {
+                    fileIn = WavFile.openWavFile(new File(args[3]));
+                    String fileOutName = new String(args[4]);
+    
+                    fileOut = WavFile.newWavFile(new File(fileOutName), fileIn.getNumChannels(), fileIn.getNumFrames(), fileIn.getValidBits(), fileIn.getSampleRate());
+                    
+                    int choice2 = args[1].equals("string") ? 1 : 2;
+    
+                    if (choice2 == 1) {
+                        String message = new String(args[2]);
+                        stegoWav.encryptMessage(message , fileIn, fileOut);
+                    } else if (choice2 == 2) {
+                        in = new FileInputStream(args[2]);
+    
+                        if (in.getChannel().size() * 8 > fileIn.getNumFrames()) {
+                            System.out.println("sorry, for this .wav file, maximum size is " + fileIn.getNumFrames() / 8);
+                            return;
+                        }
+                        stegoWav.encryptMessage(in, fileIn, fileOut);
+                    }
+                } else if (choice == 2) {
+                    fileIn = WavFile.openWavFile(new File(args[2]));
+                    int choice2 = args[1].equals("string") ? 1 : 2;
+                    
+                    if (choice2 == 1) {
+                       System.out.println(stegoWav.decryptMessage(fileIn));
+                    } else if (choice2 == 2) {
+                        out = new FileOutputStream(args[3]);
+                        stegoWav.decryptMessage(fileIn, out);
+                    }
+                }
+    
+            } finally {
+                if (fileIn != null) {
+                    fileIn.close();
+                }
+                if (fileOut != null) {
+                    fileOut.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+    
+            }
+        
+        } else if (extension.equals("mp3")) {
+            
+        }
         /**/
     }
 
