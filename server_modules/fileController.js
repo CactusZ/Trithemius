55657b5df0f601994060e3e56da94aeb56cf4515
CactusZ <igorkasevich@gmail.com>
2016-10-06 08:56:45 +0000

    closes #18

--- server_modules/fileController.js
+++ server_modules/fileController.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 router.get('/*.wav',function(req,res){
-    res.sendfile('index.html', {
-        root: './client'
-    });
+    res.download(path.resolve(__dirname + '/../results/' + req.originalUrl.split('/').pop()));
 });
 
 module.exports = router;
