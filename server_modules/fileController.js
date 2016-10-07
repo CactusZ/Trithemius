a726b1d21667cac75c79152f8feb0235c56345f2
CactusZ <igorkasevich@gmail.com>
2016-10-07 14:05:01 +0000

    resolves #30 and Java code enhancement

--- server_modules/fileController.js
+++ server_modules/fileController.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var path = require('path');
 
 router.get(['/*.wav','/*.file'],function(req,res){
-    res.download(path.resolve(__dirname + '/../results/' + req.originalUrl.split('/').pop()));
+    res.download(path.resolve('./results/' + req.originalUrl.split('/').pop()));
 });
 
 module.exports = router;
